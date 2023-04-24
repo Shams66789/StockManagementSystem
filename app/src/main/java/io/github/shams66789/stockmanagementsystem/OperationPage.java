0d212b569a5a7dc1bb5408f1ba25a17f5054f77b
Shams <rajansari66789@gmail.com>
2023-04-24 17:37:02 +0530

    Created Database and Page for adding stocks

--- app/src/main/java/io/github/shams66789/stockmanagementsystem/OperationPage.java
+++ app/src/main/java/io/github/shams66789/stockmanagementsystem/OperationPage.java
@@ -2,13 +2,46 @@ package io.github.shams66789.stockmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OperationPage extends AppCompatActivity {
+    // Declaring variables
+    TextView textView;
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation_page);
+        // linking with layout
+        textView = findViewById(R.id.textView6);
+        textView1 = findViewById(R.id.textView9);
+        textView2 = findViewById(R.id.textView10);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OperationPage.this, AddStocks.class);
+                startActivity(intent);
+            }
+        });
+
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
